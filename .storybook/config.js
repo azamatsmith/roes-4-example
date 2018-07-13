f3bb5cae7053d36f6fac9fc8e56537c67d3b3370
Matthew Smith <azamatsmith@gmail.com>
2018-07-12 18:37:58 -0600

    Remove unused file

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,9 +6,7 @@ setAddon(JSXAddon);
 const req = require.context('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
-  // require('../stories/index.js');
   req.keys().forEach(filename => req(filename));
-  // You can require as many stories as you need.
 }
 
 configure(loadStories, module);
