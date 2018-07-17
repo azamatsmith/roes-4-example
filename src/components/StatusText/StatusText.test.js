4b4d7ca7c865e71533900bd7061320f9f10ebfe9
Matthew Smith <azamatsmith@users.noreply.github.com>
2018-07-17 16:07:27 -0600

    Config - Add enzyme (#8)
    * Add unit tests for <Input /> and <StatusText />
    
    * Update <StatusText /> - make test fail for screenshot
    
    * Update <StatusText /> - fix failing test

---
+++ src/components/StatusText/StatusText.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {mount} from 'enzyme';
+import StatusText from './';
+
+describe('<StatusText />', () => {
+  const props = {
+    children: <span>An Error</span>,
+    statusType: 'error',
+  };
+
+  const defaultWrapper = mount(<StatusText {...props} />);
+
+  it('should render without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <StatusText statusType="success">Complete</StatusText>,
+      div
+    );
+  });
+
+  it('should render an error status type', () => {
+    expect(defaultWrapper.text()).toEqual('An Error');
+  });
+
+  it('should render an error status type', () => {
+    defaultWrapper.setProps({
+      statusType: 'warning',
+      children: <span>A warning</span>,
+    });
+    expect(defaultWrapper.text()).toEqual('A warning');
+  });
+});
